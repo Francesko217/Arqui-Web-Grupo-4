c9608d90ba62b2b0f348859d3545bce91111ec6b
biney-debug <spectralreturn03@gmail.com>
2026-05-04 11:37:09 -0500

    Switch to Docker runtime for Render deployment

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ if [ -n "$DATABASE_URL" ]; then
 elif [ -n "$DB_URL" ]; then
   export SPRING_DATASOURCE_URL="jdbc:postgresql${DB_URL#postgres}"
 fi
-exec java -jar target/TuTrade-0.0.1-SNAPSHOT.jar
+exec java -jar app.jar
